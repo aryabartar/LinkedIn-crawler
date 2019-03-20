dabe6e8621e1467d85070a10b14e7cad88d6008f
arya <a.khaligh@aut.ac.ir>
2019-03-20 20:00:09 +0330

    Add scroll_to_button

--- linkedin_connect_bot.py
+++ linkedin_connect_bot.py
@@ -32,7 +32,29 @@ def random_wait():
 
 
 def connect_to_alumni(page_url, driver):
+    def scroll_to_button():
+        SCROLL_PAUSE_TIME = 1
+
+        # Get scroll height
+        last_height = driver.execute_script("return document.body.scrollHeight")
+
+        while True:
+            # Scroll down to bottom
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            # Wait to load page
+            time.sleep(SCROLL_PAUSE_TIME)
+
+            # Calculate new scroll height and compare with last scroll height
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+
     driver.get(page_url)
+    random_wait()
+
+    scroll_to_button()
     random_wait()
 
     html = driver.page_source
