863af77120a3e0d3fc8d168a4ad50180f024a267
arya <a.khaligh@aut.ac.ir>
2019-03-20 17:52:20 +0330

    Add connect_to_alumni

--- linkedin_connect_bot.py
+++ linkedin_connect_bot.py
@@ -25,9 +25,25 @@ def open_linkedin(driver):
     driver.get("https://www.linkedin.com/")
     print("Set cookies.")
 
-def connect_to_alumni():
-    pass
+
+def random_wait():
+    random = randint(3, 6)
+    time.sleep(random)
+
+
+def connect_to_alumni(page_url, driver):
+    driver.get(page_url)
+    element = driver.find_element_by_xpath(
+        '/html/body/div[5]/div[6]/div[2]/div/div[2]/div/main/div[2]/ul/li[1]/div/ul/li/button')
+        '/html/body/div[5]/div[6]/div[2]/div/div[2]/div/main/div[2]/ul/li[8]/div/ul/li/button')
+
+    element.click()
+    random_wait()
+
 
 driver = webdriver.Firefox()
+driver.maximize_window()
 open_linkedin(driver)
-connect_to_alumni ()
+connect_to_alumni(
+    'https://www.linkedin.com/school/amirkabir-university-of-technology---tehran-polytechnic/people/?facetGeoRegion=se%3A0',
+    driver)
