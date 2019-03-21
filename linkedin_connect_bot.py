ed17288f8c09dab7015323d62e341df4ddb19d7c
arya <a.khaligh@aut.ac.ir>
2019-03-21 19:25:55 +0330

    Update connect_to_alumni

--- linkedin_connect_bot.py
+++ linkedin_connect_bot.py
@@ -8,6 +8,12 @@ from random import randint
 from selenium import webdriver
 
 
+def append_to_file(path, text):
+    f = open(path, "a")
+    f.write(text)
+    f.close()
+
+
 def open_linkedin(driver):
     def restore_cookie(driver):
         cookies = pickle.load(open("../cookies.pkl", "rb"))
@@ -24,28 +30,20 @@ def open_linkedin(driver):
 
 
 def random_wait():
-    random = randint(3, 6)
+    random = randint(5, 10)
     time.sleep(random)
 
 
 def connect_to_alumni(page_url, driver):
     def scroll_to_button(number_of_scrolls):
-        SCROLL_PAUSE_TIME = 1
-
-        # Get scroll height
-        last_height = driver.execute_script("return document.body.scrollHeight")
-
+        number_of_scrolls = int(number_of_scrolls)
         counter = 0
 
         while True:
+            time.sleep(1)
 
             # Scroll down to bottom
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-            # Wait to load page
-            time.sleep(SCROLL_PAUSE_TIME)
-
-            time.sleep(3)
 
             if counter == number_of_scrolls:
                 break
@@ -74,39 +72,45 @@ def connect_to_alumni(page_url, driver):
 
     alumni_number = soup.find('span', {"class": "t-20"}).text
     alumni_number = remove_first_and_last_spaces(alumni_number)
-    scroll_number = int(alumni_number.split(' ')[0]) / 12
+    scroll_number = int(int(alumni_number.split(' ')[0]) / 12)
     print("Scroll Number: ", scroll_number, " |Alumni number: ", alumni_number)
 
-    scroll_to_button(scroll_number)
+    # scroll_to_button(scroll_number)
     random_wait()
 
     html = driver.page_source
     soup = bs.BeautifulSoup(html, 'lxml')
 
-    people_html = soup.find_all('li', 'org-people-profiles-module__profile-item')
+    people_html = soup.find_all('div', {"class": "org-people-profile-card__profile-title"})
     people_number = len(people_html)
-    for i in range(1, people_number):
+
+    for i in range(0, people_number - 1):
+        # try:
         try:
-            try:
-                # Notification when connecting much.
-                driver.find_element_by_class_name('ip-fuse-limit-alert__primary-action').click()
-                print("Clicked on notification button. ")
-            except:
-                pass
-
-            connect_element = driver.find_element_by_xpath(
-                '/html/body/div[5]/div[6]/div[2]/div/div[2]/div/main/div[2]/ul/li[{id}]/div/ul/li/button'.format(id=i))
-            connect_element.click()
-
-            driver.find_element_by_xpath(
-                '/html/body/div[5]/div[7]/div/div[1]/div/section/div/div[2]/button[2]').click()
-
-            print("connected")
+            # Notification when connecting much.
+            driver.find_element_by_class_name('ip-fuse-limit-alert__primary-action').click()
+            print("Clicked on notification button. ")
         except:
-            print("Error while connecting.")
-        time.sleep(5)
+            pass
 
-    random_wait()
+        profile_name = people_html[i]
+        print(profile_name)
+
+        connect_element = driver.find_element_by_xpath(
+            '/html/body/div[5]/div[6]/div[2]/div/div[2]/div/main/div[2]/ul/li[{id}]/div/ul/li/button'.format(
+                id=i + 1))
+
+        connect_element.click()
+
+        driver.find_element_by_xpath(
+            '/html/body/div[5]/div[7]/div/div[1]/div/section/div/div[2]/button[2]').click()
+
+        print("connected")
+        # except:
+        #     print("Error while connecting.")
+        time.sleep(2)
+
+        random_wait()
 
 
 driver = webdriver.Firefox()
